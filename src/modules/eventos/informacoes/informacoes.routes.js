33ca19385641359f5b454d050226a997bc955077
Bruno Paixão <brunomrpx@gmail.com>
2016-05-06 09:42:12 -0300

    Adicionado encadeamento de views no modulo de informacoes

--- src/modules/eventos/informacoes/informacoes.routes.js
+++ src/modules/eventos/informacoes/informacoes.routes.js
@@ -1,12 +1,16 @@
 angular.module('app.modules')
     .config(function ($stateProvider) {
         $stateProvider
-            .state('informacoes', {
+            .state('eventos.informacoes', {
                 url: '/:id/informacoes',
-                templateProvider: function ($templateCache) {
-                    return $templateCache.get('eventos/informacoes/informacoes.html');
-                },
-                controller: 'informacoesCtrl',
-                controllerAs: 'vm'
+                views: {
+                    '@': {
+                        templateProvider: function ($templateCache) {
+                            return $templateCache.get('eventos/informacoes/informacoes.html');
+                        },
+                        controller: 'informacoesCtrl',
+                        controllerAs: 'vm'
+                    }
+                }
             })
     })
