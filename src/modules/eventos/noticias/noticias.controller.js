0ac691f04157ecc44815c4ac7510e9a3786bb5f7
Lucas <lucas.tavares@ntconsult.com.br>
2016-05-09 14:57:09 -0300

    Ajustes no layout e na edição de notícias.

--- src/modules/eventos/noticias/noticias.controller.js
+++ src/modules/eventos/noticias/noticias.controller.js
@@ -1,9 +1,19 @@
 angular.module('app.modules')
     .controller('noticiasCtrl', noticiasCtrl);
 
-function noticiasCtrl($scope, $modal, $templateCache, HelperService) {
+function noticiasCtrl($scope, $modal, $templateCache, ModalService) {
+    this.noticias = buscarNoticias();
     this.novaNoticia = novaNoticia;
     this.excluirNoticia = excluirNoticia;
+    this.tabs = [
+        {
+            "title": "Notícias"
+        },
+        {
+            "title": "Posts"
+        }
+    ];
+    this.tabs.activeTab = "Notícias";
 
     function novaNoticia(noticia) {
         if (!noticia) {
@@ -23,8 +33,35 @@ function noticiasCtrl($scope, $modal, $templateCache, HelperService) {
             });
     }
 
+    function buscarNoticias() {
+        var noticias = [];
+        var noticia = {};
+        for (var i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                noticia = {
+                    titulo: 'Notícia',
+                    descricao: 'No mundo atual, a complexidade dos estudos efetuados estimula a padronização dos conhecimentos estratégicos para atingir a excelência.'
+                    + 'Não obstante, a competitividade nas transações comerciais não pode mais se dissociar das condições inegavelmente apropriadas.',
+                    imagem: 'http://placehold.it/80x80',
+                    backoffice: 1
+                }
+            }
+            else {
+                noticia = {
+                    titulo: 'Post',
+                    descricao: 'No mundo atual, a complexidade dos estudos efetuados estimula a padronização dos conhecimentos estratégicos para atingir a excelência.'
+                    + 'Não obstante, a competitividade nas transações comerciais não pode mais se dissociar das condições inegavelmente apropriadas.',
+                    imagem: 'http://placehold.it/80x80',
+                    backoffice: 0
+                }
+            }
+            noticias.push(noticia);
+        }
+        return noticias;
+    }
+
     function excluirNoticia(index) {
-        HelperService.openModalConfirmation({
+        ModalService.openModalConfirmation({
             content: 'Deseja realmente excluir esta notícia?',
             showCancel: true,
             confirmFunction: function () {
